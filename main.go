975273d04deada5f1bccddd3ba5cd8af243dc5da
Alicyclic <79605349+Alicyclic@users.noreply.github.com>
2022-10-18 23:18:51 -0500

    added in bot instance

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("testing ")
+	b, _ := discordgo.New(fmt.Sprintf("Bot %s", os.Getenv("BOT_TOKEN")))
+	if err := b.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		b.Close()
+	}()
+	sc := make(chan os.Signal, 1)
+	log.Println("Bot is now running. Press CTRL-C to exit.")
+	signal.Notify(sc, os.Interrupt)
+	<-sc
+	log.Println("Bot is now closing...")
 }
